bc07ad90ac04bda43e9d720bbf33be8b748edd83
mbashish007 <mbashishoo7@gmail.com>
2024-08-30 00:36:07 +0530

    20

---
+++ 20/20.c
@@ -0,0 +1,26 @@
+/*
+Name: M B Ashish | Roll No: MT2024085
+
+20. Find out the priority of your running program. Modify the priority with nice command.
+
+Output
+$ ./a.out
+Current priority 0
+priority after nice : -1
+*/
+
+#include <stdio.h>
+       #include <unistd.h>
+#include <stdlib.h>
+       #include <sys/resource.h>
+
+int main() {
+    int priority = getpriority(PRIO_PROCESS,0);
+    printf("Current priority %d\n", priority);
+    if(priority != -19)
+    priority--;
+    priority = nice(priority);
+    printf("priority after nice : %d", priority);
+
+    return 0;
+}
