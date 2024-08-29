de59afcdb149b93445808a55d0e9036739fbfc47
mbashish007 <mbashishoo7@gmail.com>
2024-08-29 22:28:06 +0530

    18.c

---
+++ 18/18.c
@@ -0,0 +1,180 @@
+/*
+Name: M B Ashish | Roll No: MT2024085
+
+18. Write a program to perform Record locking.
+a. Implement write lock
+b. Implement read lock
+Create three records in a file. Whenever you access a particular record, first lock it then modify/access
+to avoid race condition.
+
+Output
+
+$ cat records 
+Hello
+World
+C Programming
+
+
+$ ./a.out records
+Select option
+Read Record: press 1
+Modify Record: press 2
+1
+Select record to read
+1 | 2 | 3 
+2
+Acquiring Read Lock
+option = 2
+calc offset = 20offset = 20 
+Record = World
+ 
+Released read lock
+Select option
+Read Record: press 1
+Modify Record: press 2
+2
+Select record to modify
+1 | 2 | 3 
+2
+Enter modified data
+universe
+buffer = universe
+Acquiring Write Lock
+Record = Updated Successfully 
+Released write lock
+Select option
+Read Record: press 1
+Modify Record: press 2
+^C
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define recSize 20
+
+void readRecord(int fd) {
+    struct flock lock;
+    
+    printf("Select record to read\n");
+    printf("1 | 2 | 3 \n");
+    int op;
+    scanf("%d", &op);
+
+    lock.l_type=F_RDLCK;
+    lock.l_whence= SEEK_SET;
+    lock.l_start=(recSize *(op- 1));
+    lock.l_len = recSize;
+    lock.l_pid = getpid();
+
+    char buffer[recSize];
+    printf("Acquiring Read Lock\n");
+    fcntl(fd, F_SETLKW, &lock);
+    printf("option = %d\n",op);
+    int offset = lseek(fd,(recSize *(op-1)), SEEK_SET);
+    int rB = read(fd, &buffer, recSize);
+    if(rB == -1) {
+        perror("read error\n");
+        return;
+    }
+    printf("Record = %s \n", buffer);
+
+    lock.l_type= F_UNLCK;
+    fcntl(fd, F_SETLK, &lock);
+    printf("Released read lock\n");
+
+
+}
+
+void modifyRecord(int fd) {
+    struct flock lock;
+    int option ;
+    printf("Select record to modify\n");
+    printf("1 | 2 | 3 \n");
+    scanf("%d", &option);
+
+    lock.l_type=F_WRLCK;
+    lock.l_whence= SEEK_SET;
+    lock.l_start=(recSize *(option- 1));
+    lock.l_len = recSize;
+    lock.l_pid = getpid();
+
+    char buffer[recSize];
+    printf("Enter modified data\n");
+    int rbytes = scanf("%s", buffer);
+    buffer[rbytes] = '\n';
+    printf("buffer = %s\n", buffer);
+
+    printf("Acquiring Write Lock\n");
+    fcntl(fd, F_SETLKW, &lock);
+    lseek(fd,(recSize *(option- 1)), SEEK_SET);
+
+    int rB = write(fd, &buffer, rbytes);
+    if(rB == -1) {
+        perror("write error\n");
+        return;
+    }
+    printf("Record = Updated Successfully \n");
+
+    lock.l_type= F_UNLCK;
+    fcntl(fd, F_SETLK, &lock);
+    printf("Released write lock\n");
+
+
+}
+
+
+int main(int argc, char * args[]) {
+
+    
+    int fd ;
+    fd = open(args[1], O_RDWR);
+    
+    if(fd == -1) {
+        fd = open(args[1], O_RDWR | O_CREAT, 0700) ;
+            if(fd == -1) {
+                perror("File Creation Error\n");
+                return -1;
+            }
+
+           char recs[3][20] = {
+        "Hello\n",
+        "World\n",
+        "C Programming\n"
+    };
+
+        int wB = write(fd, recs, 3*recSize);
+        if(wB == -1) {
+            perror("Write Error");
+            return -1;
+        }       
+    }
+
+    
+
+    while(1) {
+        printf("Select option\n");
+        printf("Read Record: press 1\n");
+        printf("Modify Record: press 2\n");
+        // printf("Quit anything else\n");
+        int option ;
+        scanf("%d", &option);
+
+        if(option == 1) 
+            readRecord(fd);
+        else if(option == 2)
+            modifyRecord(fd);
+        
+        // else return 0;
+
+
+    }
+
+    return 0;
+
+}
